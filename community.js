3342d17686f7fa28a2d8c874dca580a9345686a0
hongjihyang <asdfi16580@naver.com>
2022-10-20 17:35:37 +0900

    추가

--- community.js
+++
@@ -1,7 +0,0 @@
-  // 지역선택 확인
-  if(area === "지역을 선택하세요."){
-    document.getElementById("areaError").innerHTML="지역을 선택해주세요."
-    check = false
-  }else{
-    document.getElementById("areaError").innerHTML=""
-  }
